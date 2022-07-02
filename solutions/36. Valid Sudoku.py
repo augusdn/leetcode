278218e74649c32bf8c9e22035e4b2fc7fb540eb
augusdn <augusdn@gmail.com>
2022-07-02 20:34:22 +1000

    177 ms

---
+++ solutions/36. Valid Sudoku.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+​
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        boxes = defaultdict(set)
+        
+        for r in range(9):
+            for c in range(9):
+                cell = board[r][c]
+                print(cell)
+                if cell == ".":
+                    continue
+                if(cell in rows[r] or
+                   cell in cols[c] or
+                   cell in boxes[(r//3,c//3)]):
+                    return False
+                rows[r].add(cell)
+                cols[c].add(cell)
+                boxes[(r//3,c//3)].add(cell)
+        return True
